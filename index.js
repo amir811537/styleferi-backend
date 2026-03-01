501fb2d94faf9014cd501037e439962af71f7d17
amir811537 <amirhossain.bc75@gmail.com>
2026-03-01 23:54:30 +0600

    added new

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173" ,"https://styleferi.vercel.app/"], credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
